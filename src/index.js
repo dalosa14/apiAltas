21f8d4d6fe885551e43698bbf12f79edb43633fd
saul <sagar5554@gmail.com>
2021-05-06 18:48:50 +0200

    añadidos envv

--- src/index.js
+++ src/index.js
@@ -5,16 +5,16 @@ const sequelize = require("./dbs/dbConnect");
 const app = express();
 const pasosAdelopd = require('./routes/pasosAdelopd')
 const cors = require('cors');
-
+ 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
 app.use(cors({
-    origin:['https://api.infocursos.es','https://pruebas.infocursos.es ']
+    origin:['https://api.infocursos.es','https://pruebas.infocursos.es']
 }))
 app.use(helmet());
 app.use('/adelopd',pasosAdelopd)
-app.listen(3000, () => {
-  console.log("servidor en el 3000");
+app.listen(4000, () => {
+  console.log("servidor en el 4000");
   sequelize
     .sync({force:false})
     .then(() => console.log("conectado a la base de datos"))
